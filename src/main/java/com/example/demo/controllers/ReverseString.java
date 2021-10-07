39df72c9b28e5a795513903f368cd1dff423c9e3
kajetan.barwinski <222399@edu.p.lodz.pl>
2021-10-07 14:53:32 +0200

    Created controller structure for string reversal.

---
+++ src/main/java/com/example/demo/controllers/ReverseString.java
@@ -0,0 +1,12 @@
+package com.example.demo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ReverseString {
+    @GetMapping("reverse_string/{str}")
+    public String reverseString(@PathVariable String str){
+    }
+}
